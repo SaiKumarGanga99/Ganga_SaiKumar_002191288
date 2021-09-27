cac859a7c003dd60ccc4ccce63ec081b75a4777d
SaiKumarGanga99 <91031997+SaiKumarGanga99@users.noreply.github.com>
2021-09-26 22:48:06 -0400

    Add files via upload

---
+++ Assingment_002191288/src/model/Personal.java
@@ -0,0 +1,226 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author saikumarganga
+ */
+public class Personal {
+    
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String DOB;
+    private String email;
+    private String ssn;
+    private String fax;
+    private String phonenumber;
+    private String medicalrecordnumber;
+    private String healthplanbenificiary;
+    private String bankaccountnumber;
+    private String licenseplatenumber;
+    private String licensenumber;
+    private String vehciletype;
+    private String Devicetype;
+    private String uniquenumber;
+    private String linkedln;
+    private String ipaddress; 
+    private String userimage;
+    private String imagelabel;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+    
+
+    public String getMedicalrecordnumber() {
+        return medicalrecordnumber;
+    }
+
+    public void setMedicalrecordnumber(String medicalrecordnumber) {
+        this.medicalrecordnumber = medicalrecordnumber;
+    }
+
+    public String getHealthplanbenificiary() {
+        return healthplanbenificiary;
+    }
+
+    public void setHealthplanbenificiary(String healthplanbenificiary) {
+        this.healthplanbenificiary = healthplanbenificiary;
+    }
+
+    public String getBankaccountnumber() {
+        return bankaccountnumber;
+    }
+
+    public void setBankaccountnumber(String bankaccountnumber) {
+        this.bankaccountnumber = bankaccountnumber;
+    }
+
+    public String getLicenseplatenumber() {
+        return licenseplatenumber;
+    }
+
+    public void setLicenseplatenumber(String licenseplatenumber) {
+        this.licenseplatenumber = licenseplatenumber;
+    }
+
+    public String getLicensenumber() {
+        return licensenumber;
+    }
+
+    public void setLicensenumber(String licensenumber) {
+        this.licensenumber = licensenumber;
+    }
+
+    public String getVehciletype() {
+        return vehciletype;
+    }
+
+    public void setVehciletype(String vehciletype) {
+        this.vehciletype = vehciletype;
+    }
+
+    public String getDevicetype() {
+        return Devicetype;
+    }
+
+    public void setDevicetype(String Devicetype) {
+        this.Devicetype = Devicetype;
+    }
+
+    public String getDevicenumber() {
+        return uniquenumber;
+    }
+
+    public void setDevicenumber(String devicenumber) {
+        this.uniquenumber = uniquenumber;
+    }
+
+    public String getLinkedln() {
+        return linkedln;
+    }
+
+    public void setLinkedln(String linkedln) {
+        this.linkedln = linkedln;
+    }
+
+    public String getIpaddress() {
+        return ipaddress;
+    }
+
+    public void setIpaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public String getUserimage() {
+        return userimage;
+    }
+
+    public void setUserimage(String userimage) {
+        this.userimage = userimage;
+    }
+
+    public String getImagelabel() {
+        return imagelabel;
+    }
+
+    public void setImagelabel(String imagelabel) {
+        this.imagelabel = imagelabel;
+    }
+    
+    
+
+    
+}
